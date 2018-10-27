a136a48b33698d60e95efa87601bf26eb7d7f45a
cheskape <peraltacheska@gmail.com>
2018-10-28 00:18:32 +0800

    hi

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,7 @@
 Rails.application.routes.draw do
-<<<<<<< HEAD
   resources :announcements
   resources :cpd_events
-=======
   resources :enrollments
->>>>>>> 4d6524ce2badbc6d796e87429a082ff814967727
   resources :unit_modules
   devise_for :users
   resources :profiles
